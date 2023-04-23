b79df42442d51543988a51db9782f0d2783f8d0d
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-04-23 18:09:15 +0300

    added events list in the left sidebar

--- components/molecules/EventList.tsx
+++
@@ -1,5 +0,0 @@
-const EventList = () => {
-    return ( <></> );
-}
- 
-export default EventList;
--- components/organisms/Sidebar.tsx
+++ components/organisms/Sidebar.tsx
@@ -1,4 +1,4 @@
-import EventList from "../molecules/EventList";
+import CheckboxList from "../molecules/CheckboxList";
 import AvatarMenu from "../molecules/AvatarMenu";
 import MiniCalendar from "../molecules/MiniCalendar";
 import styles from '../../styles/Sidebar.module.css';
@@ -6,8 +6,12 @@ const Sidebar = () => {
     return (
         <div className={styles.sidebar}>
                 <AvatarMenu />
-            <div className="flex flex-col items-center bg-red-100">
-                <EventList />
+            <div className="flex flex-col bg-red-100">
+                <CheckboxList 
+                listItems={['Parties','Game Nights','Reminders']}
+                defaultChecked={true}
+                Colors={['text-red-600','text-blue-600','text-yellow-600']}
+                />
                 <MiniCalendar />
             </div>
         </div>
