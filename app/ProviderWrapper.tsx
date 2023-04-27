b3922bbc3fd4b676c693873eef04d826eda13ffb
gandolh <29356778+gandolh@users.noreply.github.com>
2023-04-27 11:01:04 +0300

    adaugare base path in api/auth

--- app/ProviderWrapper.tsx
+++ app/ProviderWrapper.tsx
@@ -1,7 +1,5 @@
 'use client';
 import {SessionProvider} from 'next-auth/react'
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider } from '@chakra-ui/react';
 // import { MyAccountProvider } from '../hooks/MyAccountContext'
 import "../styles/globals.css"
 export default function ProviderWrapper({
@@ -10,7 +8,7 @@ export default function ProviderWrapper({
   children: React.ReactNode;
 }) {
   return (
-   <SessionProvider>
+   <SessionProvider basePath="/mern-frontend/api/auth" >
     {children}{/* */}
    </SessionProvider>
   );
